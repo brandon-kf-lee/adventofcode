61088a8f414c2dcc1aabb5e37f0894e9c26a687d
Brandon Lee <bellee@chapman.edu>
2025-12-11 22:16:16 -0800

    Add generalizable case that can cover multiple digit ranges. Also slightly refactor to move (most) variable declarations outside of loops and fix some comments to be more clear

--- 2025/02 Dec: Gift Shop/gift_shop_pt2.c
+++ 2025/02 Dec: Gift Shop/gift_shop_pt2.c
@@ -17,10 +17,25 @@ int main() {
         return 1;
     }
 
-    unsigned long long invalid_ids = 0;
-    unsigned long long range[2] = {0, 0};
-    int c = 0;
-    int range_ind = 0; // Index of the range being read
+    unsigned long long invalid_ids = 0;      // Summation for all the invalid IDs
+    
+    int c = 0;                               // Single character being read in
+    int range_ind = 0;                       // Index of the current range being read
+    unsigned long long range[2] = {0, 0};    // Input ranges as they come in; [lower bound, upper bound]
+    int range_n[2];                          // Number of digits in a bound; [lower bound, upper bound]
+
+    unsigned long long id;                   // ID generated within the range
+    unsigned long long pattern;              // The pattern being repeated to create potential invalid IDs
+    unsigned long long pattern_n;            // Number of digits in a pattern
+    unsigned long long split;                // The number used to split IDs into their various places (e.g. tens place, hundreds place, etc.)
+
+    // (POSIX) Regex solely used to detect possible duplicate invalid IDs
+    // Regex means: Find a number with any length duplicated two or more times, and count it only if the entire string is composed of that pattern
+    regex_t regex;
+    char* regex_pattern = {"^\\([0-9]\\+\\)\\1\\+$"};  // Without escape characters: "^([0-9]+)\1+$"      
+    regcomp(&regex, regex_pattern, 0);
+
+    // Start by reading file character by character
     while(c = fgetc(ranges)){
         // If '-', start writing to upper range
         if (c == '-') {
@@ -34,117 +49,85 @@ int main() {
             continue;
         }
 
-        // If anything except a ','
+        // If anything else except for a ','
         if(c != ',' && c != EOF) {
             fprintf(stderr, "Invalid number '%c' detected in range. Exiting.\n", c);
             return 1;
         }
 
         // Use snprintf to determine how many digits in each range bound (Reference in README)
-        int range_n[2];
         range_n[0] = snprintf(0, 0, "%lld", range[0]);
         range_n[1] = snprintf(0, 0, "%lld", range[1]);
 
         printf("lower: %lld, n = %d\n", range[0], range_n[0]);
         printf("upper: %lld, n = %d\n", range[1], range_n[1]);
 
-        unsigned long long id, pattern, pattern_n, split;
-        int ranges_i, max_run = 2; // Cycling through the number of digit legnths, max_run # of times (e.g. 6-7 digits)
-
-        // If there is no change of digit count between ranges, below code should only one run once (an unfortunately hacky solution)
-        if(abs(range_n[1] - range_n[0]) < 1) max_run = 1;
-
-        // Working on the assumption that ranges will not cover 2 or more differences in number length
-        ranges_i = 0;
-        for(int temp = 0; temp < max_run; ++temp) {
+        // Get the number of times the range crosses a difference in number length + 1 (i.e. 244 (3 digits) - 4245 (4 digits))
+        int max_run = (range_n[1] - range_n[0]) + 1;
+        for(int digits = 0; digits < max_run; ++digits) {
             
-            if(temp == 1) {
+            // After evaluating the original lower range, replace it with the smallest number in the next number of digits (i.e. 345 -> 1000)
+            if(digits >= 1) {
                 range[0] = (range[0] * (int)pow(10, range_n[0]) / range[0]);
                 range_n[0]++;
+                printf("\nNEW RANGE:\nlower: %lld, n = %d\n", range[0], range_n[0]);
+                printf("upper: %lld, n = %d\n", range[1], range_n[1]);
             }
 
             // Only generate patterns based on up to half the digits in the bounds. Any pattern larger than that repeated twice would not fit. 
-            for(int possible_patterns = 0; possible_patterns < (range_n[ranges_i] / 2); ++possible_patterns) {
+            for(int possible_patterns = 0; possible_patterns < (range_n[0] / 2); ++possible_patterns) {
                 // Get the pattern starting with the first digit, then first and second, etc.
-                split = pow(10, range_n[ranges_i] - 1 - possible_patterns);
-                pattern = range[ranges_i] / split;
+                split = pow(10, range_n[0] - 1 - possible_patterns);
+                pattern = range[0] / split;
                 pattern_n = possible_patterns + 1;
                 id = 0;
 
-                //printf("first pattern: %lld\n", pattern);
-
                 // If there is any remainder when building the pattern up, pattern will not fit.
-                if(range_n[ranges_i] % pattern_n != 0) {
+                if(range_n[0] % pattern_n != 0) {
                     continue;
                 }
 
                 while (id <= range[1]){
 
-                    /*  Iteratively build pattern 
+                    /*  Iteratively use pattern to build ID
                         Based on shifting the patterns to the left and adding the patterns to the end until complete
                         Iteration number is based on how many times the pattern can fit in the original number's length
-                        1. Start with the original number of digits (range_n[ranges_i])
+                        1. Start with the lower bound number of digits (range_n[0])
                         2. Subtract the number of digits in the pattern (pattern_n)
                         3. Subtract by the size of the pattern itself (multiplied by the times already ran through)
                     */
                     id = 0;
-                    for(int j = 0; j < range_n[ranges_i] / pattern_n; ++j) {
-                        id += pattern * (int)(pow)(10, range_n[ranges_i] - pattern_n - (j * pattern_n));
+                    for(int j = 0; j < range_n[0] / pattern_n; ++j) {
+                        id += pattern * (int)(pow)(10, range_n[0] - pattern_n - (j * pattern_n));
                     }
 
                     printf("\nID: %lld\n", id);
                     printf("pattern: %lld\n", pattern);    
-                    //printf("size of pattern: %lld\n", pattern_n);
-                    printf("do not go past: %lld\n", (pattern * (int)pow(10, pattern_n) / pattern));  
 
                     // The numbers created may be invalid and start below the lower boundary (i.e. 2122-2424, but generated 2121)
                     if(id >= range[0] && id <= range[1]){
 
-                        //Deduplicate. Within the pattern, check if there are any duplicate patterns inside
-                        if (pattern_n != 1);
-                        
-                        // Without escape characters: "^([0-9]+)\1+$"
-                        char* regex_pattern = {"^\\([0-9]\\+\\)\\1\\+$"};     
-                        //printf("regex pattern: %s\n", regex_pattern);
-                        
-                        regex_t regex;
-                        int regex_ret;
-                        if((regex_ret = regcomp(&regex, regex_pattern, 0)) != 0) {
-                            char msg[100];
-                            regerror(regex_ret, &regex, msg, 100);
-                            fprintf(stderr, "Error compiling regex: %s\n", msg);                
-                            return 1;
-                        }
-
+                        // Deduplicate. Within the pattern, check if there are any duplicate patterns inside
+                        // Works on the assumption that if there is a internal repeating pattern, the pattern must have been caught by a previous ID generation step
                         char pattern_s[pattern_n];
                         snprintf(pattern_s, pattern_n + 1, "%lld", pattern);
-                        printf("pattern to check: %s\n", pattern_s);
 
                         // If pattern isn't found, not duplicate
-                        if((regex_ret = regexec(&regex, pattern_s, 0, NULL, 0)) != 0) {
+                        if((regexec(&regex, pattern_s, 0, NULL, 0)) != 0) {
                             invalid_ids += id;
                             printf("Invalid ID: %lld\n", id);         
                         }    
                         else {
                             printf("Duplicate found: %lld\n", id);
                         }
-
-                        // TODO: Free regex memory
                     }
-
 
                     // Ensure the pattern doesn't go past lower_n number of digits (i.e. pattern 9 cannot go to 10, 99 cannot go to 100)
                     if(++pattern >= (pattern * (int)pow(10, pattern_n) / pattern)) {
-                    // if(++pattern >= split) {
                         break; 
                     }
                 }
             }
-        }
-
-        // The above work may not cover 2 or more differences in number length (i.e. 11-3333 may not be covered)
-        if(abs(range_n[1] - range_n[0]) + 1 > 2) {
-            printf("WARNING: Ranges %lld-%lld pass a wide range of numbers, some invalid IDs may have been skipped.\n", range[0], range[1]);
         }
 
         // Reset range 
@@ -158,6 +141,7 @@ int main() {
 
     printf("Summation: %lld\n", invalid_ids);
     fclose(ranges);
+    regfree(&regex);
 
     return 0;
 }
