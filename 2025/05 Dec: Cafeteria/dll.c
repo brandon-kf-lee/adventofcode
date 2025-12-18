98188e1d64cf8e6b988c4cbef91caac76b329356
Brandon Lee <bellee@chapman.edu>
2025-12-17 22:37:06 -0800

    Streamline code organization: remove debug prints, add more descriptive comments

--- 2025/05 Dec: Cafeteria/dll.c
+++ 2025/05 Dec: Cafeteria/dll.c
@@ -20,126 +20,86 @@ void insert_range(range** head, long long lower, long long upper) {
     // Check if DLL is empty
     if (*head == NULL) {
         *head = create_range(lower, upper);
-        printf("empty, inserted:  (%lld-%lld)\n", (*head)->lower, (*head)->upper);
         return;
     }
 
-    // If the new node's lower is smaller than the head's, put the new node at the beginning
+    // If the new range's lower is smaller than the head's, put the new node at the beginning
     if((*head)->lower >= lower){
         insert_head(head, lower, upper);
-        printf("new head, inserted: (%lld-%lld)\n", (*head)->lower, (*head)->upper);
         deduplicate_range((*head), (*head)->next);
         return;
     }
 
-    // Create new node and step
-    range* newRange = create_range(lower, upper);
-    range* step = *head;
+    // Find the in-order place for the new node, based on lower bounds
+    // Create current range and prev (starting at the head)
+    range* current = create_range(lower, upper);
+    range* prev = *head;
 
-    printf("testing: (%lld-%lld)\n", newRange->lower, newRange->upper);
-    printf("head: (%lld-%lld)\n", step->lower, step->upper);
+    // Navigate to after a node with a smaller lower bound and before a node with a larger or same lower bound
+    while((prev->next) != NULL && 
+          (prev->lower       > current->lower || 
+           prev->next->lower < current->lower)) {
 
-    // Find the place for newRange until NULL is reached
-    while((step->next) != NULL) {
-        
-        printf("  evaluating: (%lld-%lld) (%lld-%lld) \n", step->lower, step->upper, step->next->lower, step->next->upper);
-        // After a node with a smaller lower bound, before a node with a larger or same lower bound
-        printf("  %lld >= %lld (%d) && %lld <= %lld (%d) : (%s)\n", step->lower, newRange->lower, step->lower <= newRange->lower, step->next->lower, newRange->lower, step->next->lower >= newRange->lower, (step->lower <= newRange->lower && step->next->lower >= newRange->lower) ? "found it" : "not here");
-        
-        if(step->lower       <= newRange->lower && 
-           step->next->lower >= newRange->lower) {
-            break;
-        }
-        step = step->next;
+        prev = prev->next;
     }
-
-    printf("inserting after:  (%lld-%lld)\n", step->lower, step->upper);
 
     // Insert node in place
-    newRange->next = step->next;
-    newRange->prev = step;
-    if (step->next != NULL) {
-        step->next->prev = newRange;
+    current->next = prev->next;
+    current->prev = prev;
+    if (prev->next != NULL) {
+        prev->next->prev = current;
     }
-    step->next = newRange;  
+    prev->next = current;  
 
-    deduplicate_range(step, newRange);
-
-
+    deduplicate_range(prev, current);
 }
 
-void deduplicate_range(range* step, range* current) {
-    //range* current = newRange;
-    //
+void deduplicate_range(range* prev, range* current) {
 
-    // deduplicate if lower bound == prev upper bound
-    
-    //printf("inserted: (%lld-%lld)\n", current->lower, current->upper);
-
-    while(current != NULL && step->lower <= current->lower){
-        printf("  absorb (%lld-%lld) into (%lld-%lld)?\n", current->lower, current->upper, step->lower, step->upper);
-        // Lower is in-between the previous's bounds
-        if(current->lower >= step->lower && 
-           current->lower <= step->upper) {
+    // Keep attempting to deduplicate as long as the end isn't reached and current's lower is above prev's lower
+    while(current != NULL && prev->lower <= current->lower){
+        // Absorb if current's lower is in-between the previous's bounds
+        if(current->lower >= prev->lower && 
+           current->lower <= prev->upper) {
             
-            printf("    absorbing\n");
             // Replace upper bound only if its upper is larger than previous's upper
-            if(current->upper >= step->upper) {
-                step->upper = current->upper;
+            if(current->upper >= prev->upper) {
+                prev->upper = current->upper;
             }
             // Full range is absorbed, so delete the range
-            free_range(current);
+            delete_range(current);
 
-            // After current has been freed/absorbed, continue from step
-            current = step;
+            // After current has been freed/absorbed, continue from prev
+            current = prev;
         }
-        // Shift to next node, unless it's NULL
-        if((current = current->next) == NULL || (step = current->prev) == NULL) return;
+        // Shift two pointers to the next node, unless it's NULL
+        if((current = current->next) == NULL || (prev = current->prev) == NULL) return;
     }
 }
 
-void free_range(range* node){
-    if(node == NULL) return;
-    
-    // printf("node's prev: %x\n", node->prev);
-    // printf("node's next: %x\n\n", node->next);
+void insert_head(range** head, long long lower, long long upper) {
+    range* newRange = create_range(lower, upper);
 
-    if (node->prev != NULL) {
-        node->prev->next = node->next;
+    // Check if DLL is empty
+    if (*head == NULL) {
+        *head = newRange;
+        return;
     }
-
-    if (node->next != NULL) {
-        node->next->prev = node->prev;
-    }
-
-    free(node);
+    newRange->next = *head;
+    (*head)->prev = newRange;
+    *head = newRange;
 }
 
 int find_id(range** head, long long id) {
     // Check if DLL is empty
-    if (*head == NULL) {
-        return 0;
-    }
+    if (*head == NULL) return 0;
 
-    range* step = *head;
-    do
-    {
-        if (id >= step->lower && id <= step->upper) return 1;
-
-
-    } while ((step = step->next) != NULL);
+    range* temp = *head;
+    do {
+        if (id >= temp->lower && id <= temp->upper) return 1;
+    } while ((temp = temp->next) != NULL);
 
     return 0;
-    
-
-    // while (step->next != NULL && 
-    //       (id < step->lower || 
-    //        id > step->upper)) {
-    //     step = step->next;
-    // }
-
-    // // Return 0 if next node is null, otherwise, ID range found
-    // return (!(step->next)? 0 : 1);
 }
 
 long long range_total(range* head) {
@@ -152,18 +112,31 @@ long long range_total(range* head) {
     return total;
 }
 
-void insert_head(range** head, long long lower, long long upper) {
-    // creating new node
-    range* newRange = create_range(lower, upper);
+void delete_range(range* node){
+    if(node == NULL) return;
 
-    // check if DLL is empty
-    if (*head == NULL) {
-        *head = newRange;
-        return;
+    if (node->prev != NULL) {
+        node->prev->next = node->next;
     }
-    newRange->next = *head;
-    (*head)->prev = newRange;
-    *head = newRange;
+
+    if (node->next != NULL) {
+        node->next->prev = node->prev;
+    }
+
+    free(node);
+}
+
+void delete_list(range* head) {
+    range* temp = head;
+    range* next;
+
+    if(head == NULL) return;
+
+    do {
+        next = temp->next;
+        free(temp);
+        temp = next;
+    } while(temp != NULL);
 }
 
 void print_list(range* head) {
@@ -174,17 +147,4 @@ void print_list(range* head) {
         temp = temp->next;
     }
     printf("\n\n");
-}
-
-void delete_list(range* head) {
-    range* step = head;
-    range* next;
-
-    if(head == NULL) return;
-
-    do{
-        next = step->next;
-        free(step);
-        step = next;
-    } while(step != NULL);
 }
