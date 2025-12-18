b763784fc90917330295c3fbe29cbaae90a1ac80
Brandon Lee <bellee@chapman.edu>
2025-12-17 22:34:28 -0800

    Add comments to new functions (deduplicate range & range_total), reorder function to follow a more logical order

--- 2025/05 Dec: Cafeteria/dll.h
+++ 2025/05 Dec: Cafeteria/dll.h
@@ -25,23 +25,25 @@ range* create_range(long long lower, long long upper);
 // Insert range in order (based on lower bound)
 void insert_range(range** head, long long lower, long long upper);
 
+// Absorb any neighbor's that have overlapping ranges
 void deduplicate_range(range* step, range* current);
-
-// Find potential ID within range
-int find_id(range** head, long long id);
-
-long long range_total(range* head);
 
 // Insert a node at the beginning (head)
 void insert_head(range** head, long long lower, long long upper);
 
-// Delete a range node
-void free_range(range* node);
+// Find potential ID within range
+int find_id(range** head, long long id);
 
-// Print the list in forward direction
-void print_list(range* head);
+// Sum the total valid IDs within all the ranges
+long long range_total(range* head);
+
+// Delete a range node
+void delete_range(range* node);
 
 // Free all dynamic memory allocated by list
 void delete_list(range* head);
+
+// Print the list in forward direction
+void print_list(range* head);
 
 #endif //DLL_H
