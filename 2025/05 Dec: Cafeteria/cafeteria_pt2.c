8b0cdc9571f095d3e4a084e1189369be6c9a016c
Brandon Lee <bellee@chapman.edu>
2025-12-17 22:33:34 -0800

    Remove ID evaluation portion, reorder while loop to remove break statement

--- 2025/05 Dec: Cafeteria/cafeteria_pt2.c
+++ 2025/05 Dec: Cafeteria/cafeteria_pt2.c
@@ -17,41 +17,19 @@ int main() {
     }
 
     char test_char;
-    long long lower, upper, id;
-    int total_fresh = 0;
+    long long lower, upper;
     range* head = NULL;
 
     // Build list of ranges
-    while(1) {
-        // If a '\n' newline is detected, must be the end of the ranges section
-        if((test_char = fgetc(inventory)) == '\n'){
-            break;
-        }
+    // If a '\n' newline is detected, must be the end of the ranges section
+    while((test_char = fgetc(inventory)) != '\n') {
         ungetc(test_char, inventory);
-        
         fscanf(inventory, "%lld-%lld", &lower, &upper);
-        fgetc(inventory); // Ignore the newline at the end of each range 
+        fgetc(inventory); // Ignore one newline at the end of each range 
         insert_range(&head, lower, upper);
-        print_list(head);
-    }
-
-    // Evaluate IDs
-    while(1) {
-        // If EOF is detected (or extraneous '\n'), must be the end of the item section
-        if((test_char = fgetc(inventory)) == EOF || test_char == '\n'){
-            break;
-        }
-        ungetc(test_char, inventory);
-        
-        fscanf(inventory, "%lld", &id);
-        fgetc(inventory); // Ignore the newline at the end of each id 
-        total_fresh += find_id(&head, id);
-
-        printf("%lld: %d\n", id, find_id(&head, id));
     }
    
-    printf("Fresh ingredients: %d\n", total_fresh);
-    printf("Total fresh ingredients %lld\n", range_total(head));
+    printf("Total fresh ingredient: %lld\n", range_total(head));
     fclose(inventory);
     delete_list(head);
     return 0;
