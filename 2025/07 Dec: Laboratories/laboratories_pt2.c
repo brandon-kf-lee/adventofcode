362f693df4fc841690945ee66ac0620ea829b003
Brandon Lee <bellee@chapman.edu>
2025-12-21 17:29:54 -0800

    Add completed Part 2 (with debug printouts)

---
+++ 2025/07 Dec: Laboratories/laboratories_pt2.c
@@ -0,0 +1,84 @@
+/* Advent of Code
+ * Solution for Day 7: Laboratories, Part 1
+ * Brandon Lee
+ * brandon.kf.lee@gmail.com
+ */ 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void print_line(long long* line, int line_len) {
+    for(int i = 0; i < line_len; ++i) {
+        printf("%lld", line[i]);
+    }
+}
+
+int main() {
+    FILE* diagram;
+    if((diagram = fopen("input.txt", "r")) == NULL) {
+        fprintf(stderr, "Input does not exist/cannot open! Insert an input file called input.txt in the same directory.\n");
+        return 1;
+    }
+
+    char* curr = NULL;        // Current line being analyzed (as char)
+    long long* line = NULL;   // Current line after analysis (converted to int to track where the number of worlds a beam may come from)
+    long long* prev = NULL;   // Holder for previous line
+ 
+    size_t line_len = 0;      // True line length, ignoring newlines, null terminators, and padding
+    size_t line_len_pad;      // Padded line length, for a zero on each end
+    long long timelines = 0;  // Total number of different timelines 
+    
+    // Get the first line and set respective line lengths
+    line_len = getline(&curr, &line_len, diagram) - 1;
+    line_len_pad = line_len + 2;
+
+    // 0 out prev and line, set "S" to the first beam (coming from the root world)
+    prev = calloc(line_len_pad, sizeof(long long));
+    line = calloc(line_len_pad, sizeof(long long));
+    prev[strcspn(curr, "S") + 1] = 1; // + 1 to start analysis with padding at the beginning
+
+    // Main loop: read in a new line until EOF and analyze for splitters
+    // Scan 2 extra characters (newline & null terminator)
+    while(fgets(curr, (int)(line_len) + 2, diagram)) {
+
+        printf("prev: '");
+        print_line(prev, line_len_pad);
+        printf("'\n");
+        printf("curr: '%s'\n", curr);
+
+        int pad = 0;  // For padding, to set the index for accessing prev and line one after the start
+        for(int i = 0; i < line_len; ++i) {
+            pad = i + 1;
+
+            // Set line to the number of beams coming from above, unless above is a splitter
+            line[pad] = (curr[i] == '^') ? 0 : prev[pad];
+
+            // If left is a splitter, add the number of worlds' beams going into that splitter
+            // If right is a splitter, add the number of worlds' beams going into that splitter
+            if(curr[i - 1] == '^') line[pad] += prev[pad - 1];
+            if(curr[i + 1] == '^') line[pad] += prev[pad + 1];
+        }
+
+        printf("\n  done \n");
+        printf("prev: '");
+        print_line(prev, line_len_pad);
+        printf("'\n");
+        printf("line: '");
+        print_line(line, line_len_pad);
+        printf("\n\n\n");
+
+        // Copy finished line into prev for next analysis
+        memcpy(prev, line, sizeof(long long) * line_len_pad);
+    }
+        
+    for(int i = 0; i < line_len_pad; ++i) timelines += line[i];
+
+    printf("Timelines: %lld\n", timelines);
+    fclose(diagram);
+    free(prev);
+    free(line);
+    free(curr);
+    return 0;
+}
